2e8dd3d753372196df6d189eab7032021d95e189
Aditya More <adityamore9022@gmail.com>
2024-11-21 14:40:07 +0530

    Added: UC5 Ability to add multiple person to address book

--- src/com/AddressBook/AddressBook.java
+++ src/com/AddressBook/AddressBook.java
@@ -1,43 +1,64 @@
 package com.AddressBook;
-
+import com.AddressBook.Contact;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Iterator;
+import  java.util.Scanner;
 
-public class AddressBook
-{
-    public ArrayList<Contact> contactList = new ArrayList<>();
-    public  ArrayList<Contact> contactList(Contact d)
-    {
-        contactList.add(d);
-        return contactList;
+public class AddressBook {
+    private final ArrayList<Contact> contactList = new ArrayList<>();
+
+    public void addContact(Contact contact) {
+        contactList.add(contact);
     }
-    public void editContact(String firstName, String lastName)
-    {
-        boolean isContactFound = false;
-        for (Contact contact : contactList) {
-            if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getLastName().equalsIgnoreCase(lastName))
-            {
-                isContactFound = true;
-                System.out.println("Contact found! Enter new details:");
-                Scanner sc = AddressBookMain.sc; // Use the existing scanner
-                System.out.println("Enter the new address:");
-                contact.setAddress(sc.next());
-                System.out.println("Enter the new city:");
-                contact.setCity(sc.next());
-                System.out.println("Enter the new state:");
-                contact.setState(sc.next());
-                System.out.println("Enter the new zip code:");
-                contact.setZip(sc.next());
-                System.out.println("Enter the new phone number:");
-                contact.setPhoneNumber(sc.next());
-                System.out.println("Enter the new email:");
-                contact.setEmail(sc.next());
-                System.out.println("Contact updated successfully!");
-                break;
+
+    public void viewContacts() {
+        if (contactList.isEmpty()) {
+            System.out.println("No contacts available.");
+        } else {
+            for (Contact contact : contactList) {
+                System.out.println(contact.getFirstName() + " " + contact.getLastName() + ", " +
+                        contact.getAddress() + ", " + contact.getCity() + ", " + contact.getState() + ", " +
+                        contact.getZip() + ", " + contact.getPhoneNumber() + ", " + contact.getEmail());
             }
         }
-        if (!isContactFound) {
-            System.out.println("No contact found with the given name.");
+    }
+
+    public void editContact(String firstName, String lastName) {
+        for (Contact contact : contactList) {
+            if (contact.getFirstName().equalsIgnoreCase(firstName) &&
+                    contact.getLastName().equalsIgnoreCase(lastName)) {
+                System.out.println("Contact found! Enter new details:");
+                Scanner sc = AddressBookMain.sc;
+                System.out.print("New Address: ");
+                contact.setAddress(sc.next());
+                System.out.print("New City: ");
+                contact.setCity(sc.next());
+                System.out.print("New State: ");
+                contact.setState(sc.next());
+                System.out.print("New Zip Code: ");
+                contact.setZip(sc.next());
+                System.out.print("New Phone Number: ");
+                contact.setPhoneNumber(sc.next());
+                System.out.print("New Email: ");
+                contact.setEmail(sc.next());
+                System.out.println("Contact updated successfully!");
+                return;
+            }
         }
+        System.out.println("No contact found with the given name.");
+    }
+
+    public void deleteContact(String firstName, String lastName) {
+        Iterator<Contact> iterator = contactList.iterator();
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getFirstName().equalsIgnoreCase(firstName) &&
+                    contact.getLastName().equalsIgnoreCase(lastName)) {
+                iterator.remove();
+                System.out.println("Contact deleted successfully!");
+                return;
+            }
+        }
+        System.out.println("No contact found with the given name.");
     }
 }
